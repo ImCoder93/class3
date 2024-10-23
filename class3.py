ea23407f1f5b77a849633b71f9ef717380f999d0
ImCoder93 <duanhaoran366@gmail.com>
2024-10-23 16:49:59 +0100

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,16 @@
+import pgzrun
+
+WIDTH= 300
+HEIGHT= 300
+
+def draw():
+    width=10
+    height=20
+    r=255
+    g=0
+    b=0
+    rect = rect ((100,000),(100,80))
+    screen.draw.rect(rect,(r.g.b))
+
+
+pgzrun.go()
